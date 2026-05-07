c2842ae3405a549f454c2ec3366411ee71d02310
asra-faheem124 <asrafaheem124@example.com>
2026-05-07 22:53:45 +0500

    Initialized the projrct

---
+++ android/app/src/main/kotlin/com/example/mediaid_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mediaid_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/components/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants  {
+ static const Color mainColor = Color(0xff334eff); 
+ static const Color bgColor = Color(0xff0a0d14); 
+ static const Color fieldsColor = Color(0xff29374f);
+ static const Color cardColor = Color(0xff3c4860);
+}

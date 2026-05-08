4a23ac59477293a704d70bbed6b75738345bdbbf
Asra Faheem <157874691+asra-faheem124@users.noreply.github.com>
2026-05-08 12:10:31 +0500

    Create splash.dart

---
+++ lib/splash.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:mediaid_ui/components/constants.dart';
+import 'package:mediaid_ui/components/textStyles.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorConstants.background,
+      body: Stack(
+        children: [
+          // Bottom Background Shapes
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 170,
+              decoration: BoxDecoration(
+                color: ColorConstants.primary.withOpacity(0.06),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(120),
+                  topRight: Radius.circular(120),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            bottom: -20,
+            left: -40,
+            child: Container(
+              width: 240,
+              height: 120,
+              decoration: BoxDecoration(
+                color: ColorConstants.primary.withOpacity(0.08),
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+          ),
+
+          Positioned(
+            bottom: -25,
+            right: -30,
+            child: Container(
+              width: 230,
+              height: 120,
+              decoration: BoxDecoration(
+                color: ColorConstants.primaryLight.withOpacity(0.08),
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+          ),
+
+          // Main Content
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo Box
+                  Container(
+                    width: 125,
+                    height: 125,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: ColorConstants.primary.withOpacity(0.20),
+                          blurRadius: 25,
+                          offset: const Offset(0, 10),
+                        ),
+                      ],
+                    ),
+
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Image.asset(
+                        "assets/images/logo.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 35),
+
+                  // App Name
+                  const Text(
+                    "MediAid AI",
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                      color: ColorConstants.primary,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  // Subtitle
+                  const Text(
+                    "Smart First Aid Assistant",
+                    style: AppTextStyles.subHeading,
+                    // style: TextStyle(
+                    //   fontSize: 15,
+                    //   color: ColorConstants.bodyText,
+                    //   fontWeight: FontWeight.w500,
+                    // ),
+                  ),
+
+                  const SizedBox(height: 60),
+
+                  // Loading Bar
+                  Container(
+                    width: 120,
+                    height: 6,
+                    decoration: BoxDecoration(
+                      color: ColorConstants.border,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: ColorConstants.primary,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

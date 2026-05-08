b7b4970ff7a8eb60f731a852ed8fb080185e4241
Asra Faheem <157874691+asra-faheem124@users.noreply.github.com>
2026-05-08 11:50:41 +0500

    Update constants.dart

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ColorConstants  {
- static const Color mainColor = Color(0xff334eff); 
- static const Color bgColor = Color(0xff0a0d14); 
- static const Color fieldsColor = Color(0xff29374f);
- static const Color cardColor = Color(0xff3c4860);
+class ColorConstants{
+ static const Color primary = Color(0xff199A8E); 
+ static const Color primaryLight = Color(0xff4FC3B7); 
+ static const Color primaryDark = Color(0xff0F6F66); 
+ static const Color background = Color(0xffFFFFFF); 
+ static const Color border = Color(0xffE5E7EB);
+ static const Color heading = Color(0xff111827); 
+ static const Color bodyText = Color(0xff485563); 
+ static const Color lightText = Color(0xff9CA3AF); 
+ static const Color success = Color(0xff22C55E); 
+ static const Color warning = Color(0xffEF9E0B); 
+ static const Color danger = Color(0xffEF4444);
 }
